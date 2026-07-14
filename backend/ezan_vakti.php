d7e48a8417ee71bc20cf9c01e97b62f7e7d51401
yaseminkarasu <yaseminkarasu84@gmail.com>
2026-07-14 12:52:49 +0300

    son versiyon

--- backend/ezan_vakti.php
+++ backend/ezan_vakti.php
@@ -2,7 +2,7 @@
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-
+header("Access-Control-Allow-Methods: GET");
 require_once "db.php";
 
 $il = "İstanbul";
